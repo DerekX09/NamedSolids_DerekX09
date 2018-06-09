b688a63abbf4d535183c732b54e1879c14bf6b19
Kevin Harrington <mad.hephaestus@gmail.com>
2018-06-08 22:20:54 -0400

    adding named solids

--- NamedSolids.groovy
+++ NamedSolids.groovy
@@ -1,1 +1,8 @@
-//Your code here
+double size =20
+return [
+new Dodecahedron(size).toCSG().movex(size*0),
+new Icosahedron(size).toCSG().movex(size*2),
+new Octahedron(size).toCSG().movex(size*4),
+new Tetrahedron(size).toCSG().movex(size*6),
+
+]
